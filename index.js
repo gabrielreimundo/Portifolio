bda8ca56bebb3c627db1469c39f0ce0820a8f392
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-06-29 14:53:21 -0300

    at

--- index.js
+++ index.js
@@ -38,3 +38,57 @@ const mobileNavbar = new MobileNavbar(
     ".nav-list li",
 );
 mobileNavbar.init();
+
+const acessoA = document.querySelectorAll('#a');
+const projetoA = document.querySelectorAll('.caixaProjetos>:nth-child(1)');
+
+projetoA.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        acessoA.forEach((el) => {
+            el.style.display = 'flex';
+        });
+    });
+});
+projetoA.forEach((element) => {
+    element.addEventListener("mouseout", () => {
+        acessoA.forEach((el) => {
+            el.style.display = 'none';
+        });
+    });
+});
+
+const acessoB = document.querySelectorAll('#b');
+const projetoB = document.querySelectorAll('.caixaProjetos>:nth-child(2)');
+
+projetoB.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        acessoB.forEach((el) => {
+            el.style.display = 'flex';
+        });
+    });
+});
+projetoB.forEach((element) => {
+    element.addEventListener("mouseout", () => {
+        acessoB.forEach((el) => {
+            el.style.display = 'none';
+        });
+    });
+});
+
+const acessoc = document.querySelectorAll('#c');
+const projetoc = document.querySelectorAll('.caixaProjetos>:nth-child(3)');
+
+projetoc.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        acessoc.forEach((el) => {
+            el.style.display = 'flex';
+        });
+    });
+});
+projetoc.forEach((element) => {
+    element.addEventListener("mouseout", () => {
+        acessoc.forEach((el) => {
+            el.style.display = 'none';
+        });
+    });
+});
